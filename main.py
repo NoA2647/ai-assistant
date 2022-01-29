9fbd9fbb970daf009db3b306e882982ced0c26cc
amir <amir.h.joghataei003@gmail.com>
2022-01-29 14:40:20 +0330

    add profile to main and remove teemo

--- main.py
+++ main.py
@@ -1,35 +1,40 @@
 from Manager import Manager
 from Speaker import Speaker
 from Listener import Listener
+from Profile import Profile
+import sys
 
 
 def wakeWord(listener, speaker):
     command = listener.listen()
     if 'alex' in command:
-        speaker.play("yes_sir.mp3")
+        speaker.say("hi")
         return True
     else:
         return False
 
 
-def welcome(speaker):
-    speaker.say("Hello Amir, welcome back.")
+def welcome(speaker, name):
+    speaker.say(f"Hello {name}, welcome back.")
 
 
 def run_ai():
     listener = Listener()
     speaker = Speaker()
-    manager = Manager(None, speaker)
+    profile = Profile()
+    profile.readProfile()
+    manager = Manager(profile, speaker)
     manager.getUtils()
-    welcome(speaker)
+    welcome(speaker, profile.getName())
     while True:
         while True:
             print("wake word:")
             if wakeWord(listener, speaker):
                 break
         print("listen")
+        #command = "transfer file to me"
         command = listener.listen()
         manager.query(command)
-
+        #sys.exit()
 
 run_ai()
