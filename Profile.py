5ebcc2e5a741de7121d5d1fa58afdddf2d780a3e
riven <amir.h.joghataei003@gmail.com>
2022-02-11 11:59:32 -0500

    create .profile file and manage better

--- Profile.py
+++ Profile.py
@@ -1,21 +1,59 @@
-import map
 import json
+import os
+
+
+def createProfile(path):
+    print("fill your profile")
+    name = input("name: ")
+    userName = input("userName(what you wanna AI call you): ")
+    ipAddress = input(
+        "static local ip address of your ai(if you don't have static ip address just press Enter): ")
+    email = input("email(it uses to read & send emails): ")
+    nas = input("create home server(yes/no): ")
+    nas = nas.lower()
+    if nas is "yes" or nas is "y":
+        nas = True
+    else:
+        nas = False
+
+    profile = {
+        "name": name,
+        "userName": userName,
+        "ipAddress": ipAddress,
+        "email": email,
+        "nas": nas
+    }
+
+    json_object = json.dumps(profile)
+
+    with open(path, "w") as outfile:
+        outfile.write(json_object)
+        outfile.close()
+
 
 class Profile:
 
-    _name = None
-    _userName = None
-    _ipAddress = None
-    _email = None
+    def __init__(self, mapper):
+        self.map = mapper
+        self._name = ""
+        self._userName = ""
+        self._ipAddress = ""
+        self._email = ""
+        self._nas = False
+
+        if not os.path.isfile(self.map.getProfilePath):
+            createProfile(self.map.getProfilePath())
 
     def readProfile(self):
-        path = map.getProfilePath()
+        path = self.map.getProfilePath()
         file = open(path)
         profile = json.load(file)
         self._name = profile["name"]
         self._userName = profile["userName"]
         self._ipAddress = profile["ipAddress"]
         self._email = profile["email"]
+        self._nas = profile["nas"]
+        file.close()
 
     def getName(self):
         return self._name
@@ -28,3 +66,6 @@ class Profile:
 
     def getEmail(self):
         return self._email
+
+    def getNas(self):
+        return self._nas
